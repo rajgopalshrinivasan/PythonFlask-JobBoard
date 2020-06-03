596a0ab2625cfcc6d34822b56795c5cff79603b3
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-06-03 00:00:48 -0400

    Display All Jobs

--- jobs/app.py
+++ jobs/app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/jobs")
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
